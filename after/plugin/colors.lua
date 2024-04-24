4f18d38e68ece4617e8504c0da4fa7748ba7dd0a
Aleh Belski <ABelski@bigleaf.net>
2024-04-24 11:39:21 +0200

    Changing theme, resolving conflicts

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,21 +1,48 @@
-require('rose-pine').setup({
-  disable_background = true,
-  disable_float_background = true,
+-- require('rose-pine').setup({
+--   disable_background = true,
+--   disable_float_background = true,
+-- })
+-- 
+-- function ApplyTheme(schemeId)
+--   scheme = schemeId or "rose-pine"
+--   vim.cmd.colorscheme(scheme)
+-- 
+--   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--   vim.api.nvim_create_autocmd("ColorScheme", {
+--     pattern = "*",
+--     callback = function()
+--       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--     end,
+--   })
+-- end
+-- 
+-- ApplyTheme()
+
+-- Default options:
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
 })
-
-function ApplyTheme(schemeId)
-  scheme = schemeId or "rose-pine"
-  vim.cmd.colorscheme(scheme)
-
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "*",
-    callback = function()
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end,
-  })
-end
-
-ApplyTheme()
+vim.cmd("colorscheme gruvbox")
 
--- lua/alhb/packer.lua
+++ lua/alhb/packer.lua
@@ -65,4 +65,6 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
+
+  use { "ellisonleao/gruvbox.nvim" }
 end)
